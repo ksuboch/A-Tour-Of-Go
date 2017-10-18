7f703f6c39b689d25927f7bb838bfcd1c136c94f
Kirill Suboch <3dvapper.inbox@gmail.com>
2017-10-18 13:45:40 +0400

    Multiple results

--- 003_Functions/functions.go
+++ 003_Functions/functions.go
@@ -10,9 +10,14 @@ func pAdd(x, y *int) int {
 	return *x + *y
 }
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	fmt.Println(add(42, 13))
 	a, b := new(int), new(int)
 	*a, *b = 2, 3
 	fmt.Println(pAdd(a, b))
+	fmt.Println(swap("world", "Hello"))
 }
