2a67e99f2e275e3c40fc7c30b0c33852546f2557
Kirill Suboch <3dvapper.inbox@gmail.com>
2017-10-18 13:47:23 +0400

    Named results

--- 003_Functions/functions.go
+++ 003_Functions/functions.go
@@ -14,10 +14,17 @@ func swap(x, y string) (string, string) {
 	return y, x
 }
 
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
 	fmt.Println(add(42, 13))
 	a, b := new(int), new(int)
 	*a, *b = 2, 3
 	fmt.Println(pAdd(a, b))
 	fmt.Println(swap("world", "Hello"))
+	fmt.Println(split(17))
 }
